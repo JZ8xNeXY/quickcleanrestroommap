85ca574c44352654734078f6680dfe9c04026ebf
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-05-22 21:50:37 +0900

    トイレ情報を登録するためのコントローラファイル修正

--- rails/app/controllers/api/v1/posts_controller.rb
+++ rails/app/controllers/api/v1/posts_controller.rb
@@ -8,4 +8,21 @@ class Api::V1::PostsController < ApplicationController
     post = Post.find(params[:id])
     render json: post
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :address, :content, :latitude, :longitude,
+                                 :nursing_room, :anyone_toilet, :diaper_changing_station,
+                                 :powder_corner, :stroller_accessible, :image)
+  end
 end
