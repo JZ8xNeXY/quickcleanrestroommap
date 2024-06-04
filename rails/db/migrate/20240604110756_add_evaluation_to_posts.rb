9ea8c070a3bc1d0cab21dbc8a69e565f83be27fa
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-06-04 20:09:26 +0900

    postテーブルにevaluation列を追加

---
+++ rails/db/migrate/20240604110756_add_evaluation_to_posts.rb
@@ -0,0 +1,5 @@
+class AddEvaluationToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :evaluation, :integer
+  end
+end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_18_222428) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_04_110756) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_18_222428) do
     t.boolean "stroller_accessible", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "evaluation"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
