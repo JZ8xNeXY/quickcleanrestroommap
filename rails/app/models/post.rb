c5b91a43fdbb7bf80301b8f2db6e1630ef16ed90
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-06-10 20:01:43 +0900

    緯度経度のバリデーション設定

--- rails/app/models/post.rb
+++ rails/app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true, unless: :was_attached?
+  validates :latitude, presence: true
+  validates :longitude, presence: true
   validates :evaluation, inclusion: { in: 1..5, message: "must be between 1 and 5" }
 
   def was_attached?
