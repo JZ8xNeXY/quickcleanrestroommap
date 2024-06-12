738f39271a6a70db3ce4f8049b37a05ece0685a5
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-06-13 04:58:13 +0900

    factroybotの作成

--- rails/spec/requests/api/v1/posts_spec.rb
+++ rails/spec/requests/api/v1/posts_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  describe  'GET api/v1/posts' do
+    subject {get(api_v1_posts_path)}
+
+      it "正常にレスポンスが返る" do
+        subject
+        res = JSON.parse(response.body)
+        puts(response.body)
+        puts(res)
+        expect(response).to have_http_status(:success)
+      end
+    end
 end
