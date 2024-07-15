744b7ab2f032c46b50a6514aa3d63ded77ddd8ea
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-15 22:05:58 +0900

    supabase.tsの修正

--- next/src/utils/supabase.ts
+++ next/src/utils/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_API_KEY,
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or API Key')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
