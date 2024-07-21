0ea09aae62f7e20374ad62874574b7cbd2bc61b1
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-21 17:41:49 +0900

    googleanalyticsの導入

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { CacheProvider, EmotionCache } from '@emotion/react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
 import HeaderContainer from '@/containers/HeaderContainer'
 import { RestroomProvider } from '@/context/RestRoomContext'
 import { SessionProvider } from '@/context/SessionContext'
@@ -9,6 +12,7 @@ import Footer from '@/presentationals/Footer'
 
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
+import * as gtag from '@/utils/googleAnalytics'
 import '../styles/globals.css'
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -20,19 +24,50 @@ interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps): JSX.Element {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouterChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <SessionProvider>
-          <RestroomProvider>
-            <CssBaseline />
-            <HeaderContainer />
-            <Component {...pageProps} />
-            <Footer />
-          </RestroomProvider>
-        </SessionProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <SessionProvider>
+            <RestroomProvider>
+              <CssBaseline />
+              <HeaderContainer />
+              <Component {...pageProps} />
+              <Footer />
+            </RestroomProvider>
+          </SessionProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   )
 }
