9b3b710a062059e05c143b14bebd6a1b5476efd6
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-21 21:17:23 +0900

    googleAnalytics.tsの修正

--- next/src/utils/googleAnalytics.ts
+++ next/src/utils/googleAnalytics.ts
@@ -6,7 +6,6 @@ declare global {
 }
 
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
-
 export const pageview = (url: string) => {
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_path: url,
