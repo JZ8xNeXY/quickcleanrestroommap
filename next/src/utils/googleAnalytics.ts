7c0b61c17b7589c068fe64eabe90cad6257e5e8f
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-21 17:49:14 +0900

    googleanalyticsの導入

--- next/src/utils/googleAnalytics.ts
+++ next/src/utils/googleAnalytics.ts
@@ -1,3 +1,10 @@
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag: (...args: any[]) => void
+  }
+}
+
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
 
 export const pageview = (url: string) => {
