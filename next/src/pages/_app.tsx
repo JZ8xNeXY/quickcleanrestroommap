a2b2999fd64133e61de5904f77f32c3bfa28dfdb
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-07-22 21:30:03 +0900

    _app.tsxの修正

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { CacheProvider, EmotionCache } from '@emotion/react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import Script from 'next/script'
@@ -12,8 +13,9 @@ import Footer from '@/presentationals/Footer'
 
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
-import * as gtag from '@/utils/googleAnalytics'
 import '../styles/globals.css'
+
+// app/layout.tsx
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -24,37 +26,10 @@ interface MyAppProps extends AppProps {
 
 export default function MyApp(props: MyAppProps): JSX.Element {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
-
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouterChange = (url: string) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouterChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouterChange)
-    }
-  }, [router.events])
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
- 
-           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-           `,
-        }}
-      />
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -67,7 +42,8 @@ export default function MyApp(props: MyAppProps): JSX.Element {
             </RestroomProvider>
           </SessionProvider>
         </ThemeProvider>
-      </CacheProvider>
+      </CacheProvider>{' '}
+      {GA_MEASUREMENT_ID && <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />}
     </>
   )
 }
