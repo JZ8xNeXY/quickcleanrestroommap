b07dfcf1d1a4811648aec2deb1a9bc019326ea50
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-09-02 20:21:22 +0900

    restroomInterface.tsの修正

--- next/src/interface/restroomInterface.ts
+++ next/src/interface/restroomInterface.ts
@@ -1,17 +1,17 @@
 //個別の施設情報
 export interface Restroom {
-  name: string
-  address: string
-  content: string
-  latitude: number
-  longitude: number
-  nursingRoom: boolean
-  anyoneToilet: boolean
-  diaperChangingStation: boolean
-  powderCorner: boolean
-  strollerAccessible: boolean
-  evaluation: number
-  image: string
+  name: string | null
+  address: string | null
+  content: string | null
+  latitude: number | null
+  longitude: number | null
+  nursingRoom: boolean | null
+  anyoneToilet: boolean | null
+  diaperChangingStation: boolean | null
+  powderCorner: boolean | null
+  strollerAccessible: boolean | null
+  evaluation: number | null
+  image: string | null
 }
 
 //施設情報全体
