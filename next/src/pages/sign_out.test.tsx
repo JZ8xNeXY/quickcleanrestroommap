bcbfadb98d34477536ff28eec21e145016466624
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-10-06 16:09:12 +0900

    sign_out.test.tsxの作成

---
+++ next/src/pages/sign_out.test.tsx
@@ -0,0 +1,67 @@
+import { render, waitFor } from '@testing-library/react'
+import SignOut from '@/pages/sign_out'
+import { supabase } from '@/utils/supabase'
+import { useRouter } from 'next/router'
+import { useUserState } from '@/utils/useGlobalState'
+
+
+beforeEach(() => {
+  jest.clearAllMocks()
+
+  Object.defineProperty(window, 'localStorage', {
+    value: {
+      clear: jest.fn(),
+    },
+    writable: true,
+  })
+})
+
+jest.mock('@/utils/supabase', () => ({
+  supabase: {
+    auth: {
+      signOut: jest.fn().mockResolvedValue({ error: null }), // エラーがない場合のモック
+    },
+  },
+}))
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}))
+
+jest.mock('@/utils/useGlobalState', () => ({
+  useUserState: jest.fn(),
+}))
+
+describe('SignOut', () => {
+  it('should sign out user, clear local storage, and redirect to home page', async () => {
+    const mockPush = jest.fn() 
+    const setUserMock = jest.fn() 
+
+    const mockUseRouter = useRouter as jest.Mock
+    mockUseRouter.mockReturnValue({
+      push: mockPush, 
+    })
+
+    const mockUseUserState = useUserState as jest.Mock
+    mockUseUserState.mockReturnValue([{}, setUserMock])
+
+
+    render(<SignOut />)
+
+    await waitFor(() => {
+      expect(supabase.auth.signOut).toHaveBeenCalled()
+    })
+
+    expect(localStorage.clear).toHaveBeenCalledTimes(1)
+
+    expect(setUserMock).toHaveBeenCalledWith({
+      id: 0,
+      email: '',
+      isSignedIn: false,
+      isFetched: true,
+    })
+
+    expect(mockPush).toHaveBeenCalledWith('/')
+
+  })
+})
