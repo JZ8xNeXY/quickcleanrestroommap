30f0e19f59cd11e99822070b875beb248592241b
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-10-17 13:56:41 +0900

    cypress.config.tsの修正

--- next/cypress.config.ts
+++ next/cypress.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
     screenshotOnRunFailure: true,
   },
   env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_API_KEY: process.env.NEXT_PUBLIC_SUPABASE_API_KEY,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
     baseUrl: process.env.BASE_URL,
     email: process.env.EMAIL,
     password: process.env.PASSWORD,
