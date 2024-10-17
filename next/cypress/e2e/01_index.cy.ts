8ae14aa65066b163b1dbd042b02bd05cf3d2f48f
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-10-17 13:31:07 +0900

    index.cy.tsの修正

--- next/cypress/e2e/01_index.cy.ts
+++ next/cypress/e2e/01_index.cy.ts
@@ -1,11 +1,11 @@
 describe('Index Pages', () => {
   it('should visit the index page', () => {
     cy.visit(Cypress.env('baseUrl'))
-    // 現在地アイコン
+
     cy.get('gmp-advanced-marker[aria-label="Your Location"]').should(
       'be.visible',
     )
-    //代表的なアイコン
+
     cy.get('gmp-advanced-marker[aria-label="新丸の内ビル地下2階"]').should(
       'be.visible',
     )
@@ -15,12 +15,11 @@ describe('Index Pages', () => {
     cy.get('h3').contains('Quick Clean Restroom Map')
     cy.get('h3').contains('きれいなトイレを見つけよう')
     cy.scrollTo('bottom')
-    // 現在地アイコン
 
     cy.get('gmp-advanced-marker[aria-label="Your Location"]').should(
       'be.visible',
     )
-    //代表的なアイコン
+
     cy.get('gmp-advanced-marker[aria-label="新丸の内ビル地下2階"]').should(
       'be.visible',
     )
