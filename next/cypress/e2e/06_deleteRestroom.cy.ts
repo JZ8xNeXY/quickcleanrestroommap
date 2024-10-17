75258baac84d109fd4c590abe854e55b86f8e377
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-10-17 10:28:30 +0900

    05_editRestroom.cy.ts 06_deleteRestroom.cy.tsの作成

---
+++ next/cypress/e2e/06_deleteRestroom.cy.ts
@@ -0,0 +1,27 @@
+describe('deleteRestroom', () => {
+  it('should delete restroom', () => {
+    cy.visit(Cypress.env('baseUrl') + '/sign_in')
+
+    cy.get('input[name="email"]').type(Cypress.env('email'))
+    cy.get('input[name="password"]').type(Cypress.env('password'))
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('管理者権限でログイン中')
+
+    cy.get('gmp-advanced-marker[aria-label="test name2"]')
+      .should('be.visible')
+      .click()
+
+    cy.wait(1000)
+
+    cy.get('button.MuiButtonBase-root').contains('編集する').click()
+
+    cy.wait(1000)
+
+    cy.scrollTo('bottom')
+
+    cy.get('button.MuiButtonBase-root').contains('削除する').click()
+
+    cy.get('gmp-advanced-marker[aria-label="test name2"]').should('not.exist')
+  })
+})
