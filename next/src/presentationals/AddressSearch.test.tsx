35fd3b16899c831442a37ce03e3f886bf989433c
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-08 19:23:56 +0900

    AddressSearch.test.tsxの作成

---
+++ next/src/presentationals/AddressSearch.test.tsx
@@ -0,0 +1,53 @@
+import { render, screen, waitFor, within } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { SessionProvider } from '@/context/SessionContext'
+import Header from '@/presentationals/Header'
+import '@testing-library/jest-dom'
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const mockHeaderProps = {
+  user: null,
+  isOpen: false,
+  openDrawer: jest.fn(() => jest.fn()),
+  openAddressSearchModal: false,
+  setOpenAddressSearchModal: jest.fn(),
+  sideBar: jest.fn(() => <div>SideBar Content</div>),
+}
+
+describe('AddressSearch', () => {
+  it('Should open the modal and display the modal when the modal button is clicked', async () => {
+    const { rerender } = render(
+      <SessionProvider>
+        <Header {...mockHeaderProps} />
+      </SessionProvider>,
+    )
+
+    const menuButtons = screen.getAllByLabelText('menu')
+    const addressSearchButton = menuButtons[1]
+    await userEvent.click(addressSearchButton)
+    expect(mockHeaderProps.setOpenAddressSearchModal).toHaveBeenCalledWith(true)
+
+    rerender(
+      <SessionProvider>
+        <Header {...mockHeaderProps} openAddressSearchModal={true} />
+      </SessionProvider>,
+    )
+
+    const modal = screen.getByRole('presentation')
+    expect(within(modal).getByText('住所から検索する')).toBeInTheDocument()
+
+    const searchAddress = '東京都豊島区池袋'
+    const inputElement = screen.getByLabelText('住所')
+    expect(inputElement).toBeInTheDocument()
+    expect(inputElement).toHaveAttribute('type', 'text')
+    await userEvent.type(inputElement, searchAddress)
+    expect(inputElement).toHaveValue(searchAddress)
+
+    const submitButton = screen.getByRole('button', { name: '検索する' })
+    expect(submitButton).toBeInTheDocument()
+    expect(submitButton).toHaveAttribute('type', 'submit')
+  })
+})
