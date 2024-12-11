20c2f400d22c81bc34a27bfbe590723f3bbe6384
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-12-11 15:15:33 +0900

    Header.test.tsxの修正

--- next/src/presentationals/AddressSearch.test.tsx
+++ next/src/presentationals/AddressSearch.test.tsx
@@ -3,6 +3,7 @@ import { userEvent } from '@testing-library/user-event'
 import { SessionProvider } from '@/context/SessionContext'
 import Header from '@/presentationals/Header'
 import '@testing-library/jest-dom'
+import { RestroomProvider } from '@/context/RestRoomContext'
 
 beforeEach(() => {
   jest.clearAllMocks()
@@ -34,7 +35,9 @@ describe('AddressSearch', () => {
 
     const { rerender } = render(
       <SessionProvider>
-        <Header {...mockHeaderProps} user={signedInUser} />
+        <RestroomProvider>
+          <Header {...mockHeaderProps} user={signedInUser} />
+        </RestroomProvider>
       </SessionProvider>,
     )
 
@@ -45,16 +48,18 @@ describe('AddressSearch', () => {
 
     rerender(
       <SessionProvider>
-        <Header
-          {...mockHeaderProps}
-          user={signedInUser}
-          openAddressSearchModal={true}
-        />
+        <RestroomProvider>
+          <Header
+            {...mockHeaderProps}
+            user={signedInUser}
+            openAddressSearchModal={true}
+          />
+        </RestroomProvider>
       </SessionProvider>,
     )
 
     const modal = screen.getByRole('presentation')
-    expect(within(modal).getByText('住所から検索する')).toBeInTheDocument()
+    expect(within(modal).getByText('地名から検索する')).toBeInTheDocument()
 
     const searchAddress = '東京都豊島区池袋'
     const inputElement = screen.getByLabelText('住所')
@@ -66,5 +71,60 @@ describe('AddressSearch', () => {
     const submitButton = screen.getByRole('button', { name: '検索する' })
     expect(submitButton).toBeInTheDocument()
     expect(submitButton).toHaveAttribute('type', 'submit')
+  })
+  it('Should open the modal and display the modal when the modal button is clicked', async () => {
+    const signedInUser = {
+      id: '1',
+      email: 'admin@example.com',
+      isFetched: true,
+      isSignedIn: true,
+      app_metadata: {},
+      user_metadata: {},
+      aud: 'authenticated',
+      created_at: '2023-01-01T00:00:00Z',
+    }
+
+    const { rerender } = render(
+      <SessionProvider>
+        <RestroomProvider>
+          <Header {...mockHeaderProps} user={signedInUser} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+
+    const menuButtons = screen.getAllByLabelText('menu')
+    const addressSearchButton = menuButtons[1]
+    await userEvent.click(addressSearchButton)
+    expect(mockHeaderProps.setOpenAddressSearchModal).toHaveBeenCalledWith(true)
+
+    rerender(
+      <SessionProvider>
+        <RestroomProvider>
+          <Header
+            {...mockHeaderProps}
+            user={signedInUser}
+            openAddressSearchModal={true}
+          />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+
+    const modal = screen.getByRole('presentation')
+    expect(within(modal).getByText('地名から検索する')).toBeInTheDocument()
+
+    const searchAddress = 'jaljajlajajl'
+    const inputElement = screen.getByLabelText('住所')
+    expect(inputElement).toBeInTheDocument()
+    expect(inputElement).toHaveAttribute('type', 'text')
+    await userEvent.type(inputElement, searchAddress)
+    expect(inputElement).toHaveValue(searchAddress)
+
+    const submitButton = screen.getByRole('button', { name: '検索する' })
+    expect(submitButton).toBeInTheDocument()
+    expect(submitButton).toHaveAttribute('type', 'submit')
+
+    const warningAddressSearchMessage =
+      screen.getByText('検索結果が見つかりませんでした')
+    expect(warningAddressSearchMessage).toBeInTheDocument()
   })
 })
--- next/src/presentationals/Header.test.tsx
+++ next/src/presentationals/Header.test.tsx
@@ -1,16 +1,17 @@
-import { render, screen, waitFor } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
-import { SessionProvider } from "@/context/SessionContext";
-import Header from "@/presentationals/Header";
-import "@testing-library/jest-dom";
+import { render, screen, waitFor } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { SessionProvider } from '@/context/SessionContext'
+import Header from '@/presentationals/Header'
+import '@testing-library/jest-dom'
+import { RestroomProvider } from '@/context/RestRoomContext'
 
-jest.mock("next/router", () => ({
+jest.mock('next/router', () => ({
   useRouter: jest.fn(),
-}));
+}))
 
 beforeEach(() => {
-  jest.clearAllMocks();
-});
+  jest.clearAllMocks()
+})
 
 const mockHeaderProps = {
   router: jest.fn(),
@@ -22,89 +23,101 @@ const mockHeaderProps = {
   openAddSimpleRestroomModal: false,
   setOpenAddSimpleRestroomModal: jest.fn(),
   sideBar: jest.fn(() => <div>SideBar Content</div>),
-};
+}
 
-describe("Header", () => {
+describe('Header', () => {
   it('should not display "ログイン中" when user is not signed in', () => {
     render(
       <SessionProvider>
-        <Header {...mockHeaderProps} />
-      </SessionProvider>
-    );
-    const adminText = screen.queryByText("ログイン中");
-    expect(adminText).not.toBeInTheDocument();
-  });
+        <RestroomProvider>
+          <Header {...mockHeaderProps} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+    const adminText = screen.queryByText('ログイン中')
+    expect(adminText).not.toBeInTheDocument()
+  })
 
   it('should display "ログイン中" when user is signed in', () => {
     const signedInUser = {
-      id: "1",
-      email: "admin@example.com",
+      id: '1',
+      email: 'admin@example.com',
       isFetched: true,
       isSignedIn: true,
       app_metadata: {},
       user_metadata: {},
-      aud: "authenticated",
-      created_at: "2023-01-01T00:00:00Z",
-    };
+      aud: 'authenticated',
+      created_at: '2023-01-01T00:00:00Z',
+    }
     render(
       <SessionProvider>
-        <Header {...mockHeaderProps} user={signedInUser} />
-      </SessionProvider>
-    );
-    expect(screen.getByText("ログイン中")).toBeInTheDocument();
-  });
+        <RestroomProvider>
+          <Header {...mockHeaderProps} user={signedInUser} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+    expect(screen.getByText('ログイン中')).toBeInTheDocument()
+  })
 
   it('should display "Icon"', () => {
     render(
       <SessionProvider>
-        <Header {...mockHeaderProps} />
-      </SessionProvider>
-    );
-    const menuButtons = screen.getAllByLabelText("menu");
-    const menuButton = menuButtons[0];
-    expect(menuButton).toBeInTheDocument();
-    expect(menuButton).toHaveAttribute("type", "button");
-    const menuIcon = screen.getByTestId("MenuIcon");
-    expect(menuIcon).toBeInTheDocument();
+        <RestroomProvider>
+          <Header {...mockHeaderProps} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+    const menuButtons = screen.getAllByLabelText('menu')
+    const menuButton = menuButtons[0]
+    expect(menuButton).toBeInTheDocument()
+    expect(menuButton).toHaveAttribute('type', 'button')
+    const menuIcon = screen.getByTestId('MenuIcon')
+    expect(menuIcon).toBeInTheDocument()
 
-    const addressSearchButton = menuButtons[1];
-    expect(addressSearchButton).toBeInTheDocument();
-    expect(addressSearchButton).toHaveAttribute("type", "button");
-    const addressSearchIcon = screen.getByTestId("SearchIcon");
-    expect(addressSearchIcon).toBeInTheDocument();
+    const addressSearchButton = menuButtons[1]
+    expect(addressSearchButton).toBeInTheDocument()
+    expect(addressSearchButton).toHaveAttribute('type', 'button')
+    const addressSearchIcon = screen.getByTestId('SearchIcon')
+    expect(addressSearchIcon).toBeInTheDocument()
 
-    const addLocationButton = menuButtons[2];
-    expect(addLocationButton).toBeInTheDocument();
-    expect(addLocationButton).toHaveAttribute("type", "button");
-    const addLocationIcon = screen.getByTestId("AddLocationIcon");
-    expect(addLocationIcon).toBeInTheDocument();
-  });
+    const addLocationButton = menuButtons[2]
+    expect(addLocationButton).toBeInTheDocument()
+    expect(addLocationButton).toHaveAttribute('type', 'button')
+    const addLocationIcon = screen.getByTestId('AddLocationIcon')
+    expect(addLocationIcon).toBeInTheDocument()
+  })
 
-  it("Should open the Drawer and display the sidebar when the menu button is clicked", async () => {
+  it('Should open the Drawer and display the sidebar when the menu button is clicked', async () => {
     const { rerender } = render(
       <SessionProvider>
-        <Header {...mockHeaderProps} />
-      </SessionProvider>
-    );
-    const menuButtons = screen.getAllByLabelText("menu");
-    const menuButton = menuButtons[0];
-    await userEvent.click(menuButton);
-    expect(mockHeaderProps.openDrawer).toHaveBeenCalledWith(true);
-    expect(mockHeaderProps.openDrawer).toHaveBeenCalledTimes(2); //回数がおかしい再レンダリング可能性あり
+        <RestroomProvider>
+          <Header {...mockHeaderProps} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+    const menuButtons = screen.getAllByLabelText('menu')
+    const menuButton = menuButtons[0]
+    await userEvent.click(menuButton)
+    expect(mockHeaderProps.openDrawer).toHaveBeenCalledWith(true)
+    expect(mockHeaderProps.openDrawer).toHaveBeenCalledTimes(2) //回数がおかしい再レンダリング可能性あり
     rerender(
       <SessionProvider>
-        <Header {...mockHeaderProps} isOpen={true} />
-      </SessionProvider>
-    );
-    expect(screen.getByText("SideBar Content")).toBeInTheDocument();
+        <RestroomProvider>
+          <Header {...mockHeaderProps} isOpen={true} />
+        </RestroomProvider>
+      </SessionProvider>,
+    )
+    expect(screen.getByText('SideBar Content')).toBeInTheDocument()
 
     await waitFor(() => {
       rerender(
         <SessionProvider>
-          <Header {...mockHeaderProps} isOpen={false} />
-        </SessionProvider>
-      );
-      expect(screen.queryByText("SideBar Content")).not.toBeInTheDocument();
-    });
-  });
-});
+          <RestroomProvider>
+            <Header {...mockHeaderProps} isOpen={false} />
+          </RestroomProvider>
+        </SessionProvider>,
+      )
+      expect(screen.queryByText('SideBar Content')).not.toBeInTheDocument()
+    })
+  })
+})
