6e3360d27debe7644a2523ece4d234ca6540af5b
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-25 21:20:54 +0900

    next.config.jsの修正

--- next/next.config.js
+++ next/next.config.js
@@ -8,4 +8,27 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // 全てのリクエストに適用
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://maps.gstatic.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              img-src 'self' https://quickcleanrestroommap.s3.ap-northeast-1.amazonaws.com https://maps.gstatic.com data: https:;
+              connect-src 'self' https://wxprtdliozgiizvfejql.supabase.co https://maps.googleapis.com https://maps.gstatic.com;
+              frame-src 'self';
+              font-src 'self' https://fonts.gstatic.com;
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
