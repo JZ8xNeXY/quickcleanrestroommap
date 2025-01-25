a32b6935d59fd6d986f6823fc6785ef53d1ac5b7
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-01-25 20:33:47 +0900

    editPost.ts deletePost.tsの修正

--- next/src/pages/api/editPost.ts
+++ next/src/pages/api/editPost.ts
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase URL and Key must be provided.')
+  throw new Error('Missing Supabase URL or API Key')
 }
 
 export default async function handler(
